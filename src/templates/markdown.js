19dde63a0d5555b13a430c2ddb1130e8cd9767e9
Yuki Koyama <yuki@koyama.xyz>
2019-02-22 16:41:44 +0900

    Use Container

--- src/templates/markdown.js
+++ src/templates/markdown.js
@@ -3,19 +3,20 @@ import { Link } from "gatsby"
 import React from 'react';
 import Helmet from 'react-helmet';
 import Footer from '../components/footer';
+import { Container } from 'semantic-ui-react';
 
 const Template = ({data}) => {
-  const {markdownRemark: post} = data;
-  const {frontmatter, html} = post;
-  const {title} = frontmatter;
+  const { markdownRemark: post } = data;
+  const { frontmatter, html } = post;
+  const { title } = frontmatter;
 
   return (
-      <div>
-        <Helmet title={`${title} - WISS 2019`}/>
-        <Link to="/">Home</Link> / <Link to="/call-for-papers">Call for Papers</Link>
-        <div dangerouslySetInnerHTML={{__html: html}}/>
-        <Footer />
-      </div>
+    <Container>
+      <Helmet title={`${title} - WISS 2019`}/>
+      <Link to="/">Home</Link> / <Link to="/call-for-papers">Call for Papers</Link>
+      <div dangerouslySetInnerHTML={{__html: html}}/>
+      <Footer />
+    </Container>
   );
 };
 
