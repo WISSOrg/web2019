b0e3b131d53ea219f47f3e21e7d7551e427b7101
Yuki Koyama <yuki@koyama.xyz>
2019-06-07 19:54:31 +0900

    Use for-loop to generate gird cellls

--- src/components/programcommitteegrid.js
+++ src/components/programcommitteegrid.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import { Grid, Image } from 'semantic-ui-react';
 
+class ProgramCommitteeGridCell extends Component {
+  render() {
+    return (
+      <Grid.Column width={4}>
+        <p>Test message: this space will be used as by a program-committee grid element.</p>
+      </Grid.Column>
+    );
+  }
+}
+
 export default class ProgramCommitteeGrid extends Component {
   render() {
     return (
       <Grid style={{ marginTop: '20px' }}>
-        <Grid.Column width={4}>
-          <p>Test message: this space will be used as by a program-committee grid element.</p>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <p>Test message: this space will be used as by a program-committee grid element.</p>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <p>Test message: this space will be used as by a program-committee grid element.</p>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <p>Test message: this space will be used as by a program-committee grid element.</p>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <p>Test message: this space will be used as by a program-committee grid element.</p>
-        </Grid.Column>
+        {(() => {
+          const items = [];
+          for (let i = 0; i < 14; i++) {
+            items.push(<ProgramCommitteeGridCell />);
+          }
+          return <Grid.Row>{ items }</Grid.Row>;
+        })()}
       </Grid>
     );
   }
