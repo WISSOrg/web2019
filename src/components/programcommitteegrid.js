a436e2381a41c6856d3a82284d016b7472269ec2
Yuki Koyama <yuki@koyama.xyz>
2019-06-08 09:29:31 +0900

    Use graphql to get CSV data

--- src/components/programcommitteegrid.js
+++ src/components/programcommitteegrid.js
@@ -1,26 +1,60 @@
+import { StaticQuery, graphql } from "gatsby"
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
 import ProgramCommitteeGridCell from './programcommitteegridcell';
 
-// React component for rendering the PC member grid table
-export default class ProgramCommitteeGrid extends Component {
+// React component for rendering the PC member grid table (actual implementation)
+class ProgramCommitteeGridSub extends Component {
   render() {
+    const members_data = this.props.members_data;
+
     return (
+      // Generate a grid
       <Grid style={{ marginTop: '30px' }}>
         {(() => {
-          const items = [];
-          for (let i = 0; i < 14; i++) {
-            items.push(<ProgramCommitteeGridCell
-              name='伊藤 貴之'
-              affiliation='お茶の水女子大学'
-              link='http://itolab.is.ocha.ac.jp/~itot/'
-              twitter='@1t0ocha'
-              expertise='情報可視化・グラフィックス・音楽情報処理・ビジュアルユーザインタフェース'
-              />);
-          }
+          // Generate cells
+          const items = members_data.allCommitteeCsv.edges.map((row, i) => (
+            <ProgramCommitteeGridCell
+              name={ row.node.name }
+              affiliation={ row.node.affiliation }
+              link={ row.node.link }
+              twitter={ row.node.twitter }
+              expertise={ row.node.expertise }
+              image={ row.node.image }
+              />
+          ));
           return <Grid.Row>{ items }</Grid.Row>;
         })()}
       </Grid>
+    );
+  }
+}
+
+// React component for rendering the PC member grid table (csv data handling part)
+export default class ProgramCommitteeGrid extends Component {
+  render() {
+    return (
+      // StaticQuery is a graphql functionality for non-page component
+      <StaticQuery 
+        query={graphql`
+        query {
+          allCommitteeCsv {
+            edges {
+              node {
+                name
+                affiliation
+                expertise
+                link
+                twitter
+                image
+                sort
+              }
+            }
+          }
+        }
+      `}
+      // Pass the data for the sub class
+      render={data => <ProgramCommitteeGridSub members_data={ data } />} />
     );
   }
 }
--- src/components/programcommitteegridcell.js
+++ src/components/programcommitteegridcell.js
@@ -4,15 +4,16 @@ import { Grid, Icon, Image } from 'semantic-ui-react';
 // React component for rendering each cell in the PC member grid table
 export default class ProgramCommitteeGridCell extends Component {
   render() {
-    const { name, affiliation, link, twitter, expertise } = this.props;
+    const { name, affiliation, link, twitter, expertise, image } = this.props;
 
     const link_dom = (link === '') ? null : <a href={ link }><Icon circular inverted color='grey' name='external'></Icon></a>
     const twitter_url = 'https://twitter.com/' + twitter
     const twitter_dom = (twitter === '') ? null : <a href={ twitter_url }><Icon circular inverted color='grey' name='twitter'></Icon></a>;
+    const image_path = '/pc/' + image + ".jpg";
 
     return (
       <Grid.Column width={4} style={{ paddingBottom: '40px', textAlign: 'center' }}>
-        <Image src='/pc/placeholder.jpg' size='small' rounded centered />
+        <Image src={ image_path } size='small' rounded centered />
         <p style={{ marginTop: '1em', marginBottom: '1em' }}><b>{ name }</b></p>
         <p style={{ marginTop: '0em', marginBottom: '0.4em' }}><b>所属：</b>{ affiliation }</p>
         <p style={{ marginTop: '0em', marginBottom: '0.4em' }}><b>専門：</b>{ expertise }</p>
