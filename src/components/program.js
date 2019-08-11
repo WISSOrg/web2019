aaaedf20ac600ce4ac3f79c8000140d156fad47d
Yuki Koyama <yuki@koyama.xyz>
2019-08-11 19:35:22 +0900

    Update design

--- src/components/program.js
+++ src/components/program.js
@@ -1,6 +1,6 @@
 import { StaticQuery, graphql } from "gatsby"
 import React, { Component } from 'react';
-import { Divider, Header, Image, Grid } from 'semantic-ui-react';
+import { Divider, Grid, Header, Image, List, Segment } from 'semantic-ui-react';
 
 const session_info = {
   "1": { name: "Elephant Eggs", time: "1日目 15:40—16:50", session_chair: "（TBA）", chat_chair: "（TBA）" },
@@ -33,13 +33,32 @@ class ProgramCell extends Component {
         <Grid.Row>
           <Grid.Column width={ 16 }>[{ talk_id }] <b>{ title }</b></Grid.Column>
           <Grid.Column width={ 16 }>{ author_list }</Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
           <Grid.Column width={ 4 }><Image src="https://via.placeholder.com/300x200?text=No+Image" fluid rounded /></Grid.Column>
           <Grid.Column width={ 6 } style={ paragraph_style }><b>要旨：</b>{ abstract }</Grid.Column>
           <Grid.Column width={ 6 } style={ paragraph_style }><b>採録時コメント：</b>{ review_comment }</Grid.Column>
         </Grid.Row>
       </Grid>
+    );
+  }
+}
+
+class SessionTitle extends Component {
+  render() {
+    const target_session = this.props.target_session;
+
+    const title = (target_session === "5" ? "" : "セッション" + target_session + ": ") + session_info[target_session]["name"];
+
+    return (
+      <Segment textAlign='center' secondary >
+        <Header as="h2" id={ target_session } style={{ paddingTop: "8px" }}>
+          { title }
+        </Header>
+        <List bulleted horizontal>
+          <List.Item>{ session_info[target_session]["time"] }</List.Item>
+          <List.Item>座長：{ session_info[target_session]["session_chair"] }</List.Item>
+          <List.Item>チャット座長：{ session_info[target_session]["chat_chair"] }</List.Item>
+        </List>
+      </Segment>
     );
   }
 }
@@ -66,9 +85,12 @@ class SessionCell extends Component {
       : null
     ));
 
-    const title = (target_session === "5" ? "" : "セッション" + target_session + ": ") + session_info[target_session]["name"];
-
-    return <div><Header as="h2" id={ target_session }>{ title }</Header>{ items }</div>
+    return (
+      <div>
+        <SessionTitle target_session={ target_session } />
+        { items }
+      </div>
+    );
   }
 }
 
@@ -86,7 +108,6 @@ class ProgramImpl extends Component {
           program_data={ program_data }
           key={ "session_cell_" + key }
         />);
-        array.push(<Divider key={ "session_divider_" + key }/>)
       }
 
       return array;
