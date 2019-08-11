b5c0996f879af74fcef02295cbdb98746a7e88d6
Yuki Koyama <yuki@koyama.xyz>
2019-08-11 10:49:47 +0900

    Divide program into sessions

--- src/components/program.js
+++ src/components/program.js
@@ -1,6 +1,6 @@
 import { StaticQuery, graphql } from "gatsby"
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Divider, Header, Grid } from 'semantic-ui-react';
 
 const session_info = {
   "1": { name: "Elephant Eggs", time: "1日目 15:40--16:50" },
@@ -24,7 +24,32 @@ class ProgramCell extends Component {
       type
     } = this.props;
 
-    return <div>{ title }, {author_list}</div>
+    return <Grid><Grid.Row columns={2}><Grid.Column>{ title }</Grid.Column><Grid.Column>{author_list}</Grid.Column></Grid.Row></Grid>
+  }
+}
+
+class SessionCell extends Component {
+  render() {
+    const program_data = this.props.program_data;
+    const target_session = this.props.target_session;
+
+    const items = program_data.allProgramCsv.edges.map((row, i) => (
+      target_session === row.node.session_id
+      ? <ProgramCell
+          submission_id={ row.node.submission_id }
+          talk_id={ row.node.talk_id }
+          session_id={ row.node.session_id }
+          title={ row.node.title }
+          author_list={ row.node.author_list }
+          abstract={ row.node.abstract }
+          review_comment={ row.node.review_comment }
+          image_file={ row.node.image_file }
+          type={ row.node.type }
+          />
+      : null
+    ));
+
+    return <div><Header>Session</Header>{ items }</div>
   }
 }
 
@@ -32,20 +57,23 @@ class ProgramImpl extends Component {
   render() {
     const program_data = this.props.program_data;
 
-    const items = program_data.allProgramCsv.edges.map((row, i) => (
-      <ProgramCell
-        submission_id={ row.node.submission_id }
-        talk_id={ row.node.talk_id }
-        session_id={ row.node.session_id }
-        title={ row.node.title }
-        author_list={ row.node.author_list }
-        abstract={ row.node.abstract }
-        review_comment={ row.node.review_comment }
-        image_file={ row.node.image_file }
-        type={ row.node.type }
-        />
-    ));
-    return <div><Header>Hello World!</Header>{ items }</div>
+    const session_cells = (() => {
+      var array = [];
+
+      for (var key in session_info)
+      {
+        array.push(<SessionCell
+          target_session={ key }
+          program_data={ program_data }
+          key={ "session_cell_" + key }
+        />);
+        array.push(<Divider />)
+      }
+
+      return array;
+    })();
+
+    return <div><Header>Program</Header>{ session_cells }</div>
   }
 }
 
