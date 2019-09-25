31bb64cf3c81287b8d5fc0b3778ee71738d6af46
Yuki Koyama <yuki@koyama.xyz>
2019-09-25 17:56:33 +0900

    Replace a relative path with an absolute path to fix #8

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import SeoImage from '../images/logo/sns-card.png';
 
 export default () => (
   <Helmet>
     <meta name="description" content="WISSは、2泊3日の泊り込み形式で、インタラクティブシステムにおける未来を切り拓くような新しいアイディア・技術を議論するワークショップです。この分野において国内でもっともアクティブな学術会議のひとつであり、例年150名以上の参加者が朝から深夜まで活発で意義深い情報交換をおこなっています。さまざまなバックグラウンドを持った方々からの積極的な論文投稿および参加をお待ちしております。" />
 
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={SeoImage} />
+    <meta name="twitter:image" content="https://www.wiss.org/WISS2019/sns-card.png" />
     <meta name="twitter:title" content="WISS 2019: 第27回インタラクティブシステムとソフトウェアに関するワークショップ" />
     <meta name="twitter:description" content="WISSは、2泊3日の泊り込み形式で、インタラクティブシステムにおける未来を切り拓くような新しいアイディア・技術を議論するワークショップです。この分野において国内でもっともアクティブな学術会議のひとつであり、例年150名以上の参加者が朝から深夜まで活発で意義深い情報交換をおこなっています。さまざまなバックグラウンドを持った方々からの積極的な論文投稿および参加をお待ちしております。" />
     <meta name="twitter:url" content="https://www.wiss.org/WISS2019/" />
