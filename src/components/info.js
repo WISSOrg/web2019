e236d6cb72d49f259c84fcab3fe2e12cdf5de757
Yuki Koyama <yuki@koyama.xyz>
2019-09-27 21:10:12 +0900

    Add link to wiss2020

--- src/components/info.js
+++ src/components/info.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { List, Message } from 'semantic-ui-react';
 
 const messages = [
-  <List.Item key="1"><b>2019/09/27:</b> WISS 2019は終了しました。来年はTHE HAMANAKO（静岡県浜松市）にて<a href="">WISS 2020</a>を開催予定です。</List.Item>,
+  <List.Item key="1"><b>2019/09/27:</b> WISS 2019は終了しました。来年はTHE HAMANAKO（静岡県浜松市）にて<a href="https://www.wiss.org/WISS2020/">WISS 2020</a>を開催予定です。</List.Item>,
 ];
 
 export default class Info extends Component {
